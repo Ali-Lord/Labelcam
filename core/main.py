f78a4273de8fa6c27d28d08d8cda7f93fe18cf2b
ali <ali@alihost.localhost>
2020-02-24 00:29:46 +0800

    added videos and photos selection screen

---
+++ core/main.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+def start():
+    app = QApplication([])
+
+    window = QWidget()
+    layout = QVBoxLayout()
+
+    photos_btn = QPushButton('Label photos')
+    videos_btn = QPushButton('Label videos')
+    layout.addWidget(photos_btn)
+    layout.addWidget(videos_btn)
+    photos_btn.clicked.connect(on_photos_clicked)
+    videos_btn.clicked.connect(on_videos_clicked)
+
+    window.setLayout(layout)
+    window.show()
+    app.exec_()
+
+def on_photos_clicked():
+    # TODO: return photos state & log
+    print('"Label photos" clicked.')
+    return
+
+def on_videos_clicked():
+    # TODO: return videos state & log
+    print('"Label videos" clicked.')
+    return
+    
+    
+start()
+
+
